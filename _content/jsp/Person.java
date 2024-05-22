980f7de3e37413eede2ee0c755f713d3a4dddd8d
kimheegu <chkorea11@gmail.com>
2024-05-22 22:55:06 +0900

    Add files via upload

---
+++ _content/jsp/Person.java
@@ -0,0 +1,27 @@
+package dao;
+
+public class Person {
+	private int id=20170585;
+	private String name="±Ë»Ò±∏";
+	
+	public Person() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
